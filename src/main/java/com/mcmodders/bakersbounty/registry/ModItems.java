a02cb939dd7ea3ee980556a1914f4a558ce80688
ThomasMcDonald904 <naynaytom3000@gmail.com>
2025-09-22 23:52:09 -0400

    Added basic bread with recipe and placeholder art

--- src/main/java/com/mcmodders/bakersbounty/registry/ModItems.java
+++ src/main/java/com/mcmodders/bakersbounty/registry/ModItems.java
@@ -3,6 +3,7 @@ package com.mcmodders.bakersbounty.registry;
 import com.mcmodders.bakersbounty.BakersBounty;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.IEventBus;
@@ -12,7 +13,10 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.common.NeoForge;
 
-public class ModItems {
+import java.util.List;
+
+public class
+ModItems {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(BakersBounty.MODID);
 
@@ -23,6 +27,12 @@ public class ModItems {
     public static final DeferredItem<Item> WHEAT_FLOUR = ITEMS.registerSimpleItem(
             "wheat_flour",
             new Item.Properties());
+
+
+    // Add wheat flour item
+    public static final DeferredItem<Item> BREAD = ITEMS.registerSimpleItem(
+            "bread",
+            new Item.Properties().food(new FoodProperties.Builder().nutrition(4).saturationModifier(3).build()));
 
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, BakersBounty.MODID);
 
